b3d9a4ca8e65e8da3e58920ead74b04163255be0
Constantin <constantin.chtanko@etu.formation-aftec.com>
2021-09-10 00:28:38 +0200

    complete deployment

--- core/migrations/2__deploy_contracts.js
+++ core/migrations/2__deploy_contracts.js
@@ -1,6 +1,8 @@
 const Factory = artifacts.require("UniswapV2Factory.sol");
+const Token1 = artifacts.require("Token1.sol");
+const Token2 = artifacts.require("Token2.sol");
 
-//choose the address use by the deplyment
+//choose the address use by the deployment
 //array of addresses available to us, by default the fisrt one is gonna
 //be used by default as a deployment
 module.exports = async function (deployer, network, addresses) {
@@ -18,9 +20,17 @@ module.exports = async function (deployer, network, addresses) {
       token2Address = '';
   } else {
       //testnet deployment 
+      await deployer.deploy(Token1);
+      await deployer.deploy(Token2);
+      //give reference to these tokens
+      const token1 = await Token1.deployed();
+      const token2 = await Token2.deployed();
+      //save the addresses
+      token1Address = token1.address;
+      token2Address = token2.address;
   }
 
   //now we can execute function to the smart contract
   //get 2 token addresses of the smart contract
-  await factory.createPair();
+  await factory.createPair(token1Address, token2Address);
 };
---
+++ periphery/migrations/1_initial_migration.js
@@ -0,0 +1,31 @@
+const Migrations = artifacts.require("Migrations");
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+};
+
+/*
+
+const Router = artifacts.require("UniswapV2Router02.sol");
+const WETH = artifacts.require("WETH.sol");
+
+module.exports = async function (deployer, network) {
+    //pointer of the wrapped ether
+    let weth;
+    //address found in ganache terminal 
+    const FACTORY_ADDRESS = '0xb0A516F89958f73926af3e353b2Fcd301305F705';
+    //depliy on mainnet we don't need to deploy the wrapped ether because it is already exist
+    //gonna be connected to a deployed version
+    //test if we deply on the mainnet or testnet
+    if(network === 'mainnet') {
+        //found on etherscan
+        weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+    } else {
+        await deployer.deploy(WETH);
+        weth = await WETH.deployed();
+    }
+    await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
+};
+
+
+*/
